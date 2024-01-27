7a385d7adc763bd622bddfa7f7b120b54bdd319e
927138 <jiyh1541@gmail.com>
2024-01-27 14:29:23 +0900

    remove : unused import

--- src/main/java/com/nhnacademy/edu/minidooray/taskapi/repository/TaskTagRepository.java
+++ src/main/java/com/nhnacademy/edu/minidooray/taskapi/repository/TaskTagRepository.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.edu.minidooray.taskapi.repository;
 
 import com.nhnacademy.edu.minidooray.taskapi.domain.TaskTag;
-import com.nhnacademy.edu.minidooray.taskapi.dto.tag.TagResponse;
 import com.nhnacademy.edu.minidooray.taskapi.dto.tasktag.TagIdAndName;
 import com.nhnacademy.edu.minidooray.taskapi.dto.tasktag.TaskTagResponse;
 import java.util.List;
